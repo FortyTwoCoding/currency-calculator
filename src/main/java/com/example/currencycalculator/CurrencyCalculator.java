ec3d1df81b5fc3f39efc11417240bcca75bd288f
Timeler1 <tim.jordi@besonet.ch>
2022-01-10 14:39:01 +0100

    Tim

--- src/main/java/com/example/currencycalculator/CurrencyCalculator.java
+++ src/main/java/com/example/currencycalculator/CurrencyCalculator.java
@@ -1,10 +1,13 @@
 package com.example.currencycalculator;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,9 +19,27 @@ public class CurrencyCalculator extends Application {
         Parent root = fxmlLoader.load();
         Scene insideofstage = new Scene(root);
         stage.setTitle("Currency Calculator");
+        IntroSceneController controller = fxmlLoader.getController();
         stage.getIcons().add(new Image(getClass().getResourceAsStream("dogeicon.png")));
         stage.setScene(insideofstage);
         stage.show();
+        insideofstage.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent){
+
+                if (keyEvent.getCode() == KeyCode.ENTER){
+                    String text = controller.textAbbreviation.getText();
+                    double value;
+                    try {
+                        value = main.getvalue(text);
+                    }
+                    catch (Exception e){
+                        value = 0.0;
+                    }
+                    System.out.println(value);
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/currencycalculator/IntroSceneController.java
+++ src/main/java/com/example/currencycalculator/IntroSceneController.java
@@ -1,17 +1,21 @@
 package com.example.currencycalculator;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class IntroSceneController {
     @FXML
-    Button goButton;
+    public Button goButton;
     @FXML
-    TextField textAbbreviation;
+    public TextField textAbbreviation;
 
 
     public void buttonclicked(ActionEvent event) throws IOException {
