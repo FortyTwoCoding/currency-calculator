f954874bf5b32fdc987498f48712250cd881005c
Linus <linus.haenni@which.ch>
2022-01-10 13:27:27 +0100

    Ig weiss doch ni la mi la si

--- src/main/java/com/example/currencycalculator/CurrencyCalculator.java
+++ src/main/java/com/example/currencycalculator/CurrencyCalculator.java
@@ -2,6 +2,7 @@ package com.example.currencycalculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,14 +11,20 @@ import java.io.IOException;
 public class CurrencyCalculator extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CurrencyCalculator.class.getResource("hello-view.fxml"));
-        Scene insideofstage = new Scene(fxmlLoader.load(), 800, 500);
+        FXMLLoader fxmlLoader = new FXMLLoader(CurrencyCalculator.class.getResource("graphsc1.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene insideofstage = new Scene(root);
         stage.setTitle("Currency Calculator");
         stage.setScene(insideofstage);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        try {
+            launch(args);
+        }
+        catch(Exception exception){
+            exception.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/example/currencycalculator/IntroSceneController.java
@@ -0,0 +1,18 @@
+package com.example.currencycalculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class IntroSceneController {
+    @FXML
+    Button goButton;
+    @FXML
+    TextField textAbbreviation;
+
+
+    public void buttonclicked(ActionEvent event){
+        System.out.println("button was clicked.");
+    }
+}
